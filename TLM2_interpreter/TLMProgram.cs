1fd07126bc168f798eccf068357873d42ff54053
SchaafLaura <schaaf.laura92@gmail.com>
2025-05-16 10:19:14 +0200

    idea for instructions with languageExt

--- TLM2_interpreter/TLMProgram.cs
+++ TLM2_interpreter/TLMProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace TLM2_interpreter;
+﻿using LanguageExt;
+
+namespace TLM2_interpreter;
 internal sealed class TLMProgram(List<TLMFunction> functions)
 {
     private Dictionary<char, TLMFunction> _funcDict = functions.ToDictionary(f => f.Name);
@@ -7,32 +9,58 @@ internal sealed class TLMProgram(List<TLMFunction> functions)
     private TLMProgram? _parent;
     private TLMProgram? _child;
     
+    // initial instruction-set is hardcoded
+    // during program execution, new things can get bound or old things overridden
+    private readonly Dictionary<char, Either<Action<TLMProgram>, TLMProgram>> _instructionSet = new()
+    {
+        {'A', (Action<TLMProgram>) CoolMethod},
+        {'B', (Action<TLMProgram>) AnotherCoolMethod},
+        {'C', (Action<TLMProgram>) AnotherOne},
+    };
+    
     public void Step()
     {
+        // get next instruction
         _currentFunction.Step();
-        var instr = _currentFunction.CurrentInstruction;
+        var i = _currentFunction.CurrentInstruction;
         
         // convert to actual instruction
-        /*
-         kind of has to be here, because other Programs might have
-         different instruction sets
-         
-         dict:
-         char -> instruction
-         
-         BUT
-         
-         instruction might be some predefined function 
-         OR
-         instruction might be output of another program? (or however self-modification will work)
-         
-         maybe languageExt for either/or thingies?
-         but what if its three things? four? probably not tho.
-         
-         
-         */
-        
+        var instr = GetInstruction(i);
+
         // execute instructions
+        instr.Match(
+            some => some.Match(
+                a => a(this),
+                p => p.DoSomething()),
+            ()   => { /* do nothing */ });
+    }
+
+    private Option<Either<Action<TLMProgram>, TLMProgram>> GetInstruction(char c)
+    {
+        return _instructionSet[c] switch
+        {
+            null        => Option<Either<Action<TLMProgram>, TLMProgram>>.None,
+            var instr   => instr
+        };
+    }
+    
+    public void DoSomething()
+    {
+        
+    }
+    
+    private static void CoolMethod(TLMProgram p)
+    {
+        
+    }
+
+    private static void AnotherCoolMethod(TLMProgram p)
+    {
+        
+    }
+
+    private static void AnotherOne(TLMProgram p)
+    {
         
     }
 }
